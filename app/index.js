91f052e1ec02cb1986594ba661efed8aca62c629
Antal István Miklós <istvan.m.antal@gmail.com>
2014-05-25 11:45:35 +0100

    Better JSHint config

--- app/index.js
+++ app/index.js
@@ -1,54 +1,54 @@
 'use strict';
-var util = require('util');
-var path = require('path');
+//var util = require('util');
+//var path = require('path');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
-var chalk = require('chalk');
+//var chalk = require('chalk');
 
 
 var NodeAppGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
+    init: function () {
+        this.pkg = require('../package.json');
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
-    });
-  },
+        this.on('end', function () {
+            if (!this.options['skip-install']) {
+                this.installDependencies();
+            }
+        });
+    },
 
-  askFor: function () {
-    var done = this.async();
+    askFor: function () {
+        var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the marvelous NodeApp generator!'));
+        // Have Yeoman greet the user.
+        this.log(yosay('Welcome to the marvelous NodeApp generator!'));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+        var prompts = [{
+            type: 'confirm',
+            name: 'someOption',
+            message: 'Would you like to enable this option?',
+            default: true
+        }];
 
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+        this.prompt(prompts, function (props) {
+            this.someOption = props.someOption;
 
-      done();
-    }.bind(this));
-  },
+            done();
+        }.bind(this));
+    },
 
-  app: function () {
-    this.mkdir('app');
-    this.mkdir('app/templates');
+    app: function () {
+        this.mkdir('app');
+        this.mkdir('app/templates');
 
-    this.copy('_package.json', 'package.json');
-    this.copy('_bower.json', 'bower.json');
-  },
+        this.copy('_package.json', 'package.json');
+        this.copy('_bower.json', 'bower.json');
+    },
 
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
-  }
+    projectfiles: function () {
+        this.copy('editorconfig', '.editorconfig');
+        this.copy('jshintrc', '.jshintrc');
+    }
 });
 
 module.exports = NodeAppGenerator;
